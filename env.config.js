cf643a4afc8246eab1e24f5c5cbffd80c98f0433
eduardo cortijo <ecortijo05@gmail.com>
2025-12-30 00:15:20 -0500

    Primer commit: estructura inicial del sistema de vigilancia

---
+++ env.config.js
@@ -0,0 +1,6 @@
+const AZURE_CONFIG = {
+            endpoint: "https://eastus.api.cognitive.microsoft.com", 
+            key: "efadb4e445ad4846879a2cd520363e6f",
+            projectId: "c8faa822-84a7-4839-b0c1-065c987071a9",
+            iteration: "Iteration1" 
+        };
